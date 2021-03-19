8f8d604a5e6645eaf936754f6477a476135483f1
zreoIf <760904154@qq.com>
2021-03-19 14:30:59 +0800

    Unty 基础工程搭建

---
+++ GuardHome/Assets/Scripts/Scenes/Launch/Launcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    void Awake()
+    {
+        StartCoroutine(InitData());
+    }
+
+    IEnumerator InitData()
+    {
+        yield return null;
+    }
+}
